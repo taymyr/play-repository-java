147ce34de73efa614e8a15c44ffa23a64aa36a7e
Sergey Morgunov <hostage@me.com>
2020-10-15 03:22:13 +0300

    Migrate from Maven to Gradle

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,16 @@
+@Suppress("ObjectPropertyName")
+object Versions {
+    const val kotlin = "1.3.70"
+    const val scalaBinary = "2.13"
+    const val lagom = "1.6.4" // "1.5.5"
+    const val play = "2.8.2"
+    const val hibernateVersion = "5.3.6.Final"
+    const val ktlint = "0.32.0"
+    const val `ktlint-plugin` = "8.0.0"
+    const val jacoco = "0.8.4"
+    const val kotlintest = "3.1.10"
+    const val h2 = "1.4.197"
+    const val `nexus-staging` = "0.21.2"
+    const val `nexus-publish` = "0.4.0"
+    const val dokka = "0.10.1"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+include("api", "jpa")
+
+rootProject.name = "play-repository-java"
+project(":api").name = "play-repository-api-java"
+project(":jpa").name = "play-repository-jpa-java"
